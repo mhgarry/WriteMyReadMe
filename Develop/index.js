5a39b48ffeb407c3420ec0e42a540d358ab39690
Matt <mhgarry92@gmail.com>
2023-03-29 19:02:35 -0400

    Made Prompts for README

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,43 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = 
+
+inquirer.prompt([
+    {   
+        type: "input",
+        name: "Title",
+        message: "Enter your README title here"
+        
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Enter a description of your applicaiton here"
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Enter installion instructions for your application here"
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Enter usage information for your application here"
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "Enter contribution guidelines here"
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Enter your test instructions here"
+    } 
+]).then((answersObj) =>
+console.log(answersObj)
+);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
