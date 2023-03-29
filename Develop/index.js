c417a530f12450eb0b6faf5a7b59a265fde9806f
Matt <mhgarry92@gmail.com>
2023-03-29 18:41:29 -0400

    intial

---
+++ Develop/index.js
@@ -0,0 +1,14 @@
+// TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
