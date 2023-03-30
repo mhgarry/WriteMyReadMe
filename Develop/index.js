e683ee754600c22f89e6b8c05148845591219f4a
Matt <mhgarry92@gmail.com>
2023-03-29 23:28:15 -0400

    formatted readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,50 +1,82 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
 const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = 
 
 inquirer.prompt([
     {   
         type: "input",
-        name: "Title",
-        message: "Enter your README title here"
+        name: "title",
+        message: "Enter your project title here"
         
     },
     {
         type: "input",
-        name: "Description",
-        message: "Enter a description of your applicaiton here"
+        name: "description",
+        message: "Enter a detailed description of your project"
     },
     {
         type: "input",
-        name: "Installation",
-        message: "Enter installion instructions for your application here"
+        name: "motivation",
+        message: "What was your motivation for building this project?"
     },
     {
         type: "input",
-        name: "Usage",
-        message: "Enter usage information for your application here"
+        name: "problems",
+        message: "What problems does you project solve?"
     },
     {
         type: "input",
-        name: "Contributing",
-        message: "Enter contribution guidelines here"
+        name: "installation",
+        message: "Enter any installion instructions for your project"
     },
     {
         type: "input",
-        name: "Tests",
-        message: "Enter your test instructions here"
-    } 
-]).then((answersObj) =>
-console.log(answersObj)
-);
+        name: "usage",
+        message: "Enter detailed instructions on how to use your project"
+    },
+    {
+        type: "input",
+        name: "features",
+        message: "Enter your projects features"
+    },
+    {
+        type: "input",
+        name: "screenshot",
+        message: "Add a screenshot using ![alt text](assets/images/screenshot.png)"
+    },
+    {  
+        type: "input",
+        name: "credit",
+        message: "Enter any contributers and sources you used while building your project"
+
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter any tests you have written for your project"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Enter any ways in which other developers can contribute to your project"
+    },
+    {
+        type: "input",
+        name: "contact",
+        message: "Your email and github incase users have any questions"
+    }])
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
+
 // TODO: Create a function to initialize app
 function init() {}
+
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,62 @@
+const fs = require('fs')
+const inqurier = require('inquirer')
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  //https://choosealicense.com/
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title} 
 
+  ## Table of Contents 
+
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Features](#features)
+  - [Screenshot](#screenshot)
+  - [Credit](#credit)
+  - [Tests](#tests)
+  - [How to Contribute](#contributing)
+  - [Contact Me] (#contact)
+  - [License] (#license)
+          
+  ## Description 
+
+  ## Installation
+
+  ## Usage
+
+  ## Features
+
+  ## Screenshot
+
+  ## Credit
+
+  ## Tests
+
+  ## How to Contribute 
+
+  ## Contact Me
+  
+  ## License
 `;
 }
 
