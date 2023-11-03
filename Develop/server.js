516c53b24eead46c259b15d09cc7ed4aac66371c
Matthew Garry <mhgarry92@gmail.com>
2023-11-03 00:51:19 -0400

    create server to server to pass content between client and server to server generated readme file

---
+++ Develop/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8756;
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.use(express.static('public')); //this is how the server knows where to look for the files 
+
+app.post('/generate-reademe', (req, res) => {
+    console.log(req.body);
+    const userData = req.body; // get the user data by accessing the req.body object
+
+    const readmeContent = generateMarkdown(userData); //send the generated README back to the client in form of a generated README file 
+    res.set({
+        'Content-Disposition': 'attachment; filename=README.md', // this is how the server knows what to name the file
+        'Content-Type': 'text/markdown' // this how the server knows what type of file to send back to client 
+    });
+    res.send(readmeContent); // send the generated README back to the client in the form of a useable generated readme file 
+});
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
+
