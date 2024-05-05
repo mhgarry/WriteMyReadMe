e5a003624e352c876d52e311e48ef05a4b1a98cd
mhgarry <mhgarry92@gmail.com>
2024-05-05 01:26:50 -0400

    reset to previous state

--- Develop/index.js
+++ Develop/index.js
@@ -1,91 +1,53 @@
 const fs = require('fs');
-const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
-//Create an array of questions for user input
-const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'Enter your application title here',
-  },
-  {
-    type: 'input',
-    name: 'Introduction',
-    message: 'Enter your application introduction here',
-  },
-  {
-    //used for license information https://choosealicense.com/
-    type: 'list',
-    message: 'Choose a license for your application',
-    name: 'license',
-    choices: [
-      'Apache License 2.0',
-      'MIT License',
-      'ISC License',
-      'GNU AGPLv3',
-      'GNU GPLv3',
-      'GNU LGPLv3',
-      'Mozilla Public License 2.0',
-      'The Unlicense',
-      'Boost Software License 1.0',
-    ],
-  },
-  {
-    type: 'input',
-    name: 'Features',
-    message: 'What features does your application include?',
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'How does a user install your application?',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'How does a user use your application?',
-  },
-  {
-    type: 'input',
-    name: 'required packages',
-    message:
-      'What packages are required if any to run this application? What purposes do these packages serve for this application?',
-  },
-  {
-    type: 'input',
-    name: 'contribute',
-    message: 'How can other developers contribute to your project?',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'what is your email in case the user has questions?',
-  },
-  {
-    type: 'input',
-    message: 'What is your github account link?',
-    name: 'github',
-  },
-];
+const inquirer = require('inquirer');
+const fuzzy = require('fuzzy');
 
-//will write our file to a new file called GENERATEDREADME.md
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (error) => {
-    if (error) {
-      return console.log('Please input all data');
-    }
-    console.log('Writing your README');
-  });
-}
+// an async function that receives keeps track and assigns the user's input to the corresponding license
+const assignCheckbox = async () => {
+  try {
+    await inquirer.prompt([
+      {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license for your application',
+        choices: checkboxes,
+      },
+    ]);
+  } catch {
+    console.log('Please select a license');
+    return;
+  }
+};  // an async function that awaits the user to submit their choice of license for their application, once it receives the color inputted by the user, it will map the color to the corresponding license stored in the  content array
+assignCheckbox();  // calls the assignCheckbox function
 
-// this function is called to initialize the app when
-// a user runs the application
-function init() {
-  inquirer.prompt(questions).then(function (userData) {
-    console.log(userData);
-    writeToFile('GENERATEDREAEDME.md', generateMarkdown(userData));
-  });
-}
 
-//Function call to initialize app the intialize funciton and run all of our funcitons
-init();
+
+class LicenseData  {
+  prototype(name, color, url) {
+    this.name = name;
+    this.color = color;
+    this.url = url;
+  };
+};  // class that will hold the properties of the license properties to be used in our checkboxes and for rendering the license section of the README
+
+const renderLicenseSection = [
+{  apacheLicense:  new LicenseData('Apache License 2.0', '#2A0C4E', 'https://img.shields.io/badge/License-Apache%202.0-blue.svg') },
+{  mitLicense: new LicenseData('MIT License', '#7B3F00', 'https://img.shields.io/badge/License-MIT-yellow.svg') },
+{  iscLicense: new LicenseData('ISC License', '#E0A800', 'https://img.shields.io/badge/License-ISC-blue.svg') },
+{  gnuAPLicense:  new LicenseData('GNU AGPLv3', '#87B38D', 'https://img.shields.io/badge/License-AGPL_v3-blue.svg') },
+{  gnuGPLicense:  new LicenseData('GNU GPLv3', '#F0E3CA', 'https://img.shields.io/badge/License-GPLv3-blue.svg') },
+{  gnuLPLicense:  new LicenseData('GNU LGPLv3', '#DE6B48', 'https://img.shields.io/badge/License-LGPL_v3-blue.svg') },
+{  gnuLGLicense:  new LicenseData('Mozilla Public License 2.0', '#9B1D20', 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg') },
+{  uniclaseLicense:  new LicenseData('The Unlicense', '#3D315B', 'https://img.shields.io/badge/license-Unlicense-blue.svg') },
+{  boostLicense:  new LicenseData('Boost Software License 1.0', '#708D81', 'https://img.shields.io/badge/License-Boost_1.0-lightblue.svg') }
+];  // an array of objects associated with the licenses that will be used to render the license section of the README
+
+// a function that takes the user's inputed licesnse and maps the renderLicenseSection object based on the user's input compared to the object's name property
+//   renderLicenseSection.map((dataObject) => {
+//     (dataObject.name === license) ? dataObject : '';
+//     return dataObject;
+//   }
+// );  // maps the user's input to the corresponding license object in the renderLicenseSection array
+
+// an async function that will
